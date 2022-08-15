b26aea0795fb3df06852f94195e26855b4941788
Landon George <landon.robert.george@gmail.com>
2022-08-15 18:37:11 -0500

    Use empty struct as key values

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,20 +1,24 @@
 package isogram
 
 import (
-    "strings"
+	"strings"
 )
 
 func IsIsogram(word string) bool {
-	letters := make(map[string]int)
+	letters := make(map[string]struct{})
+	wordLower := strings.ToLower(word)
+	type empty struct{}
 	var charString string
-	for _, char := range strings.ToLower(word) {
+	for _, char := range wordLower {
 		charString = string(char)
 		if charString == "-" || charString == " " {
 			continue
 		}
-		letters[charString] += 1
-		if letters[charString] > 1 {
+		_, exists := letters[charString]
+		if exists {
 			return false
+		} else {
+			letters[charString] = empty{}
 		}
 	}
 	return true
