bbf152e19f95315bf054fbabd027a3891e9e4889
LandonRGeorge <landon.robert.george@gmail.com>
2023-03-20 10:26:18 -0500

    Add exercise

---
+++ airport-robot/airport_robot.go
@@ -0,0 +1,31 @@
+package airportrobot
+
+import "fmt"
+
+type Greeter interface {
+	LanguageName() string
+	Greet(string) string
+}
+
+func SayHello(name string, g Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s", g.LanguageName(), g.Greet(name))
+}
+
+type Italian struct{}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+func (i Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s!", name)
+}
+
+type Portuguese struct{}
+
+func (p Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (p Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Olá %s!", name)
+}
